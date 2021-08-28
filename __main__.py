4aa987125cb0dc29e95ec512d8d66978210d5c51
hereioz <66379803+hereioz@users.noreply.github.com>
2021-08-28 05:10:48 +0300

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -2,9 +2,10 @@ from tkinter import messagebox
 import tkinter
 import time
 
-
+window = tkinter.Tk()
+window.withdraw()
 messagebox.showinfo("by hereioz","this simple project created by hereioz.\n\ninstagram: https://instagram.com/hereioz\ngithub: https://github.com/hereioz")
-
+window.destroy()
 
 try:
     open("settings.txt",'r')
@@ -15,17 +16,21 @@ except:
 
 try:
     import GUI
-except:
+except ImportError:
     messagebox.showerror("ERROR!","Missing GUI.py file!\nquitting..") 
     time.sleep(0.75)
     exit(0)
+except Exception as Error:
+    messagebox.showerror("ERROR!",f"{Error}")
 
 try:
     import auto_clicker
-except:
+except ImportError:
     messagebox.showerror("ERROR!","Missing auto_clicker.py file!\nquitting..")
     time.sleep(0.75)
     exit(0)
+except Exception as Error:
+    messagebox.showerror("ERROR!",f"{Error}")
 
 
 GUI.main()
